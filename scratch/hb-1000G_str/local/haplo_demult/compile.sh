95d0f0aa4873f7945bd54b5d6996df5be19c530c
Hanno Hildenbrandt <hilddenbrandt.hanno@gmail.com>
2025-09-21 09:59:03 +0200

    nop

--- scratch/hb-1000G_str/local/haplo_demult/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-output=$1
-g++ -O3 -o ${output/.cpp/.o} $output -lgzstream -I/scratch/hb-1000G_str/local/gzstream -L/scratch/hb-1000G_str/local/gzstream -I/usr/include -std=gnu++11 -lz -Wall
